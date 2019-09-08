77ad468e9726208915cd91e993f499b538042e8c
WarDimension <49086905+WarDimension@users.noreply.github.com>
2019-09-08 12:00:30 +0800

    add song.js

---
+++ js/song.js
@@ -0,0 +1,50 @@
+const songsData = [
+  {
+    "title": "Nightmare",
+    "img": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/db9b0a59-1832-436d-be35-c4da781cf1c8/dd0nu9b-aa886b13-21f0-4e20-97ff-432e36ffd150.png",
+    "url": [{
+      "itunes": "https://itunes.apple.com/us/album/nightmare-single/1341371794?uo=4",
+      "deezer": "https://www.deezer.com/us/album/55904362",
+      "googlePlay": "https://play.google.com/store/music/album?id=Bx2khbzglch2k53gaph2a5txe2i",
+      "spotify": "https://play.spotify.com/album/3WAX7N3BOU42zPZeqw6DR3",
+      "youtubeMusic": "https://music.youtube.com/playlist?list=OLAK5uy_lXgyWybrogtK35zeAvapBoYzlSEhpQRLE",
+      "amazon": "https://www.amazon.com/dp/B079CW27VN"
+    }]
+  },
+  {
+    "title": "Abandoned",
+    "img": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/db9b0a59-1832-436d-be35-c4da781cf1c8/dd0nuzi-a72748c8-2a0f-4447-87f2-64129f670cb4.png",
+    "url": [{
+      "itunes": "https://itunes.apple.com/us/album/abandoned-single/1360087542?uo=4",
+      "deezer": "https://www.deezer.com/us/album/59088612",
+      "googlePlay": "https://play.google.com/store/music/album?id=Bsrds6nhmjjtjfgt4fnhfzhywfm",
+      "spotify": "https://play.spotify.com/album/41rRnGetiwSJqA2DvPigvc",
+      "youtubeMusic": "https://music.youtube.com/playlist?list=OLAK5uy_nWYcbbuKkXWcjx507xQM9VJln5jtT3znQ",
+      "amazon": "https://www.amazon.com/dp/B07BHF8JWG"
+    }]
+  },
+  {
+    "title": "Let's do it..",
+    "img": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/db9b0a59-1832-436d-be35-c4da781cf1c8/dd0nv9q-aa942c03-a069-4e8b-9d84-bd15640c22b9.png",
+    "url": [{
+      "itunes": "https://itunes.apple.com/us/album/lets-do-it-single/1360088411?uo=4",
+      "deezer": "https://www.deezer.com/us/album/59088622",
+      "googlePlay": "https://play.google.com/store/music/album?id=Bxo7szbrrae5uk6wgyzhy5gtzi4",
+      "spotify": "https://play.spotify.com/album/0svQYsVqzHe2aRXmlnH2KE",
+      "youtubeMusic": "https://music.youtube.com/playlist?list=OLAK5uy_nqIQrGsNxxU6KTEpu0rQVRogIFGnwtY7o",
+      "amazon": "https://www.amazon.com/dp/B07BHKP9YT"
+    }]
+  },
+  {
+    "title": "",
+    "img": "",
+    "url": [{
+      "itunes": "",
+      "deezer": "",
+      "googlePlay": "",
+      "spotify": "",
+      "youtubeMusic": "",
+      "amazon": ""
+    }]
+  }
+];
