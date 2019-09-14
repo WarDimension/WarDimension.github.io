c858ac3ea66fc4b5bfef201e6482b9801dc570dc
WarDimension <49086905+WarDimension@users.noreply.github.com>
2019-09-14 17:02:26 +0800

    Update cover.js

--- js/cover.js
+++ js/cover.js
@@ -8,11 +8,11 @@ const coversData = [
   },
   {
     "title": "Basket Case (Green Day Cover)",
-    "img": "",
-    "img1": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/db9b0a59-1832-436d-be35-c4da781cf1c8/ddg5rg0-49e8fca6-8140-4851-858c-60240653bd63.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2RiOWIwYTU5LTE4MzItNDM2ZC1iZTM1LWM0ZGE3ODFjZjFjOFwvZGRnNXJnMC00OWU4ZmNhNi04MTQwLTQ4NTEtODU4Yy02MDI0MDY1M2JkNjMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.Yhya2Lun9dgoazkMarpCNu3BrAWrBktZHZu5aT08ZzE",
+    "img": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/db9b0a59-1832-436d-be35-c4da781cf1c8/ddg5tl3-e28538ee-f857-4c4d-8214-9dd47fb4820e.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2RiOWIwYTU5LTE4MzItNDM2ZC1iZTM1LWM0ZGE3ODFjZjFjOFwvZGRnNXRsMy1lMjg1MzhlZS1mODU3LTRjNGQtODIxNC05ZGQ0N2ZiNDgyMGUucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.fPSEcrBzyjqSnfTfP-_94C8rXR-X--qvKhfaMPJVgV0",
+    "img1": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/db9b0a59-1832-436d-be35-c4da781cf1c8/ddg5tqu-802ec553-eb73-45f0-af42-579dd7840b8a.png/v1/fill/w_1280,h_1280,q_80,strp/basket_case_ft__hatsune_miku__green_day_cover__by_wardimension_ddg5tqu-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTI4MCIsInBhdGgiOiJcL2ZcL2RiOWIwYTU5LTE4MzItNDM2ZC1iZTM1LWM0ZGE3ODFjZjFjOFwvZGRnNXRxdS04MDJlYzU1My1lYjczLTQ1ZjAtYWY0Mi01NzlkZDc4NDBiOGEucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.7Fbcl2842sy4q3CA9eH8-3iOC5HeA14GwQXEcePC1wA",
     "url": [{
       "youtube": "https://www.youtube.com/playlist?list=PLjU3ePCiIkWlSWJ2OCrd8xNypfuWzuXzi",
-      "bandLab": ""
+      "bandLab": "https://www.bandlab.com/wardimension/collections/e3355859-cdd6-e911-b5e9-28187831ee7a"
     }]
   }
 ];
