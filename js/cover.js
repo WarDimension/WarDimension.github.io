f144231f0630f20275ff8110b5f669575ae25aa0
WarDimension <49086905+WarDimension@users.noreply.github.com>
2019-09-14 16:58:38 +0800

    add cover songs

---
+++ js/cover.js
@@ -0,0 +1,18 @@
+const coversData = [
+  {
+    "title": "バーチャルボーイ ft. 初音ミク (nano.RIPE Cover)",
+    "img": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/db9b0a59-1832-436d-be35-c4da781cf1c8/ddg5rg0-49e8fca6-8140-4851-858c-60240653bd63.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2RiOWIwYTU5LTE4MzItNDM2ZC1iZTM1LWM0ZGE3ODFjZjFjOFwvZGRnNXJnMC00OWU4ZmNhNi04MTQwLTQ4NTEtODU4Yy02MDI0MDY1M2JkNjMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.Yhya2Lun9dgoazkMarpCNu3BrAWrBktZHZu5aT08ZzE",
+    "url": [{
+      "youtube": "https://www.youtube.com/playlist?list=PLjU3ePCiIkWnmvop0AXGm4KcyuPAF6GGT"
+    }]
+  },
+  {
+    "title": "Basket Case (Green Day Cover)",
+    "img": "",
+    "img1": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/db9b0a59-1832-436d-be35-c4da781cf1c8/ddg5rg0-49e8fca6-8140-4851-858c-60240653bd63.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2RiOWIwYTU5LTE4MzItNDM2ZC1iZTM1LWM0ZGE3ODFjZjFjOFwvZGRnNXJnMC00OWU4ZmNhNi04MTQwLTQ4NTEtODU4Yy02MDI0MDY1M2JkNjMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.Yhya2Lun9dgoazkMarpCNu3BrAWrBktZHZu5aT08ZzE",
+    "url": [{
+      "youtube": "https://www.youtube.com/playlist?list=PLjU3ePCiIkWlSWJ2OCrd8xNypfuWzuXzi",
+      "bandLab": ""
+    }]
+  }
+];
--- js/song.js
+++ js/song.js
@@ -1,128 +1,3 @@
-const songsData = [
-  {
-    "title": "Nightmare",
-    "img": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/db9b0a59-1832-436d-be35-c4da781cf1c8/dd0nu9b-aa886b13-21f0-4e20-97ff-432e36ffd150.png",
-    "url": [{
-      "itunes": "https://itunes.apple.com/us/album/nightmare-single/1341371794?uo=4",
-      "deezer": "https://www.deezer.com/us/album/55904362",
-      "googlePlay": "https://play.google.com/store/music/album?id=Bx2khbzglch2k53gaph2a5txe2i",
-      "spotify": "https://play.spotify.com/album/3WAX7N3BOU42zPZeqw6DR3",
-      "youtubeMusic": "https://music.youtube.com/playlist?list=OLAK5uy_lXgyWybrogtK35zeAvapBoYzlSEhpQRLE",
-      "amazon": "https://www.amazon.com/dp/B079CW27VN"
-    }]
-  },
-  {
-    "title": "Abandoned",
-    "img": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/db9b0a59-1832-436d-be35-c4da781cf1c8/dd0nuzi-a72748c8-2a0f-4447-87f2-64129f670cb4.png",
-    "url": [{
-      "itunes": "https://itunes.apple.com/us/album/abandoned-single/1360087542?uo=4",
-      "deezer": "https://www.deezer.com/us/album/59088612",
-      "googlePlay": "https://play.google.com/store/music/album?id=Bsrds6nhmjjtjfgt4fnhfzhywfm",
-      "spotify": "https://play.spotify.com/album/41rRnGetiwSJqA2DvPigvc",
-      "youtubeMusic": "https://music.youtube.com/playlist?list=OLAK5uy_nWYcbbuKkXWcjx507xQM9VJln5jtT3znQ",
-      "amazon": "https://www.amazon.com/dp/B07BHF8JWG"
-    }]
-  },
-  {
-    "title": "Let's do it..",
-    "img": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/db9b0a59-1832-436d-be35-c4da781cf1c8/dd0nv9q-aa942c03-a069-4e8b-9d84-bd15640c22b9.png",
-    "url": [{
-      "itunes": "https://itunes.apple.com/us/album/lets-do-it-single/1360088411?uo=4",
-      "deezer": "https://www.deezer.com/us/album/59088622",
-      "googlePlay": "https://play.google.com/store/music/album?id=Bxo7szbrrae5uk6wgyzhy5gtzi4",
-      "spotify": "https://play.spotify.com/album/0svQYsVqzHe2aRXmlnH2KE",
-      "youtubeMusic": "https://music.youtube.com/playlist?list=OLAK5uy_nqIQrGsNxxU6KTEpu0rQVRogIFGnwtY7o",
-      "amazon": "https://www.amazon.com/dp/B07BHKP9YT"
-    }]
-  },
-  {
-    "title": "FruityDimension",
-    "img": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/db9b0a59-1832-436d-be35-c4da781cf1c8/dd0nvsc-ca29a2d3-0f02-4a12-b5e1-1a389004ce99.png",
-    "url": [{
-      "itunes": "https://itunes.apple.com/us/album/fruity-dimension-ep/1402708218?uo=4",
-      "deezer": "https://www.deezer.com/album/68426701",
-      "googlePlay": "http://play.google.com/store/music/album?id=B5m6iaph56ok5ubw66ytjuarfxm",
-      "spotify": "https://play.spotify.com/album/4jMZzRKQIQUSKmb36r8bdi",
-      "youtubeMusic": "https://music.youtube.com/playlist?list=OLAK5uy_ngbTWfAhv0pPJyAkV1yCMy4RSPqoAVIq8",
-      "amazon": "https://www.amazon.com/dp/B07DY5LJ4H"
-    }]
-  },
-  {
-    "title": "&#2384;",
-    "img": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/db9b0a59-1832-436d-be35-c4da781cf1c8/dd0nw5r-26508513-69a1-40ce-843e-a8544b6a614a.png",
-    "url": [{
-      "itunes": "https://itunes.apple.com/us/album/%E0%A5%90-ep/1411653135?uo=4",
-      "deezer": "https://www.deezer.com/album/68198331",
-      "googlePlay": "https://play.google.com/store/music/album?id=Bbzuzvn6qokux5xvogiljt6pw6a",
-      "spotify": "https://play.spotify.com/album/1tqkRMhkZryFa1rZbFdrV4",
-      "youtubeMusic": "https://music.youtube.com/playlist?list=OLAK5uy_nLp99MftlDnTU0g-8i5WX1acavz-kbsMw",
-      "amazon": "https://www.amazon.com/dp/B07FK8WN57"
-    }]
-  },
-  {
-    "title": "NoHope_",
-    "img": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/db9b0a59-1832-436d-be35-c4da781cf1c8/dd0nweg-838ec8bb-2e88-444f-83f6-164f222f453e.png",
-    "url": [{
-      "itunes": "https://itunes.apple.com/us/album/no-hope-single/1436219355?uo=4",
-      "deezer": "https://www.deezer.com/album/73130572",
-      "googlePlay": "https://play.google.com/store/music/album?id=B6fokvgicwiabwbmrbrkjdynzla",
-      "spotify": "https://play.spotify.com/album/0PBOg2kVA8XWCN1SiH0u69",
-      "youtubeMusic": "https://music.youtube.com/playlist?list=OLAK5uy_kd8QzOX73fXWIlrDffjkdDlJEKEpTzLM0",
-      "amazon": "https://www.amazon.com/dp/B07HBCH7BM"
-    }]
-  },
-  {
-    "title": "Hyouka",
-    "img": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/db9b0a59-1832-436d-be35-c4da781cf1c8/dd0nwok-ae360c0a-8d01-4673-8f2a-a32505f1dd59.png",
-    "url": [{
-      "itunes": "https://itunes.apple.com/us/album/hyouka-single/1436060545?uo=4",
-      "deezer": "https://www.deezer.com/album/73005342",
-      "googlePlay": "https://play.google.com/store/music/album?id=Blkjf3nbm5bessmmwrba4ef37m4",
-      "spotify": "https://play.spotify.com/album/6ZWMwloBk5ke2Jyfsm7Ix4",
-      "youtubeMusic": "https://music.youtube.com/playlist?list=OLAK5uy_mPLcT60QoFNTP6RFsNmCMT-SDqr5uJCoQ",
-      "amazon": "https://www.amazon.com/dp/B07H9GB6MR"
-    }]
-  },
-  {
-    "title": "Vboo",
-    "img": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/db9b0a59-1832-436d-be35-c4da781cf1c8/dd0nwtg-ff4e6837-4b0e-4b9d-89fa-7fca63a1aa65.png",
-    "url": [{
-      "itunes": "https://itunes.apple.com/us/album/vboo/1450558969?uo=4",
-      "deezer": "https://www.deezer.com/album/86688942",
-      "googlePlay": "https://play.google.com/store/music/album?id=Blhtd2yeecbc57q4zz64rzqccte",
-      "spotify": "https://play.spotify.com/album/4RFOcMBbcXLdIBXY5MpkDJ",
-      "youtubeMusic": "https://music.youtube.com/playlist?list=OLAK5uy_l3m8d3HT5Ai49qhDTLKYAu0Jxl26O9jMk",
-      "amazon": "https://www.amazon.com/dp/B07N359FPM",
-      "rocksmith": "http://ignition.customsforge.com/search/browse?filters=%7B%22artist%22%3A%22WarDimension%22%2C%22album%22%3A%22Vboo%22%7D"
-    }]
-  },
-  {
-    "title": "Neko da",
-    "img": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/db9b0a59-1832-436d-be35-c4da781cf1c8/dd0nx3e-cabbf13b-741b-4932-94b5-af77fbe6889b.png",
-    "url": [{
-      "itunes": "https://itunes.apple.com/us/album/neko-da-ep/1454056720?uo=4",
-      "deezer": "https://www.deezer.com/album/88337072",
-      "googlePlay": "https://play.google.com/store/music/album?id=Byxjdogbce3ykshrr46s6cf6gha",
-      "spotify": "https://play.spotify.com/album/6Mj29kVSrR5cUwgGF0HThQ",
-      "youtubeMusic": "https://music.youtube.com/playlist?list=OLAK5uy_kiuXgQTZcTi0c9gC34_Ec3Lye6MwZ_FFw",
-      "amazon": "https://www.amazon.com/dp/B07NZ92B4G"
-    }]
-  },
-  {
-    "title": "BandLab",
-    "img": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/db9b0a59-1832-436d-be35-c4da781cf1c8/ddg3q36-5b8b1f0b-5760-4bcf-a309-716e083d8693.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2RiOWIwYTU5LTE4MzItNDM2ZC1iZTM1LWM0ZGE3ODFjZjFjOFwvZGRnM3EzNi01YjhiMWYwYi01NzYwLTRiY2YtYTMwOS03MTZlMDgzZDg2OTMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.bXGCm9UE07L0ArUZNsTWeZhh9o1hoW5sg2EII4V985k",
-    "url": [{
-      "itunes": "https://music.apple.com/us/album/band-lab/1469635289?uo=4",
-      "deezer": "https://www.deezer.com/us/album/106191142",
-      "googlePlay": "https://play.google.com/store/music/album?id=Bnt6efpi42stsd3pkjzqt2jm4ra",
-      "spotify": "https://play.spotify.com/album/7cYdhAfrl5z92fa9ddGMpW",
-      "youtubeMusic": "https://music.youtube.com/playlist?list=OLAK5uy_nsRJ9w9SFCy3fZr5wXzOuaZrT9tzxLveA",
-      "amazon": "https://www.amazon.com/dp/B07THS4L2B",
-      "bandLab": "https://www.bandlab.com/wardimension/collections/54611ac3-28d6-e911-b5e9-28187831ee7a"
-    }]
-  }
-];
-
 function platformTemplate(song){
   return `
     ${song.url[0].itunes != undefined ?
@@ -143,6 +18,11 @@ function platformTemplate(song){
     ${song.url[0].spotify != undefined ?
       `<a class="platform-url" href="${song.url[0].spotify}"  target="aboutblank">
         <img class="platform-img" src="./images/Spotify.png"/>Spotify
+      </a>` : ``
+    }
+    ${song.url[0].youtube != undefined ?
+      `<a class="platform-url" href="${song.url[0].youtube}"  target="aboutblank">
+        <img class="platform-img" src="./images/YouTube.png"/>YouTube
       </a>` : ``
     }
     ${song.url[0].youtubeMusic != undefined ?
@@ -172,7 +52,7 @@ function songTemplate(song){
   return `
     <div class="content">
       <div class="album-container">
-        <img class="song-img" src="${song.img}" alt="${song.title} Album Art"/><!--
+        <img class="song-img" src="${song.img}" alt="${song.title} Album Art" ${song.img1 != undefined ? `onmouseover="src='${song.img1}'" onmouseout="src='${song.img}'"` : ``}/><!--
         --><b class="song-title-disk"><p class="song-title">${song.title}</p></b>
       </div>
       <div class="platform-container">
@@ -183,4 +63,39 @@ function songTemplate(song){
   `;
 }
 
-document.getElementById("content-container").innerHTML = `${songsData.map(songTemplate).join("")}`;
+var original = true;
+var content = document.getElementById("content-container");
+var originalButton = document.getElementById("original");
+var coverButton = document.getElementById("cover");
+songDisplay();
+
+function songDisplay(){
+  if(original){
+    content.innerHTML = `${songsData.map(songTemplate).join("")}`;
+  }
+  else{
+    content.innerHTML = `${coversData.map(songTemplate).join("")}`;
+  }
+}
+
+function originalSong(){
+  original = true;
+  songDisplay();
+  originalButton.style.background = "#000";
+  originalButton.style.color = "#fff";
+  originalButton.style.cursor = "default";
+  coverButton.style.background = "#fff";
+  coverButton.style.color = "#000";
+  coverButton.style.cursor = "pointer";
+}
+
+function coverSong(){
+  original = false;
+  songDisplay();
+  originalButton.style.background = "#fff";
+  originalButton.style.color = "#000";
+  originalButton.style.cursor = "pointer";
+  coverButton.style.background = "#000";
+  coverButton.style.color = "#fff";
+  coverButton.style.cursor = "default";
+}
