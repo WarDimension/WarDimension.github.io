b1fdd06db274b6c493351f4cf13b79991c258246
WarDimension <49086905+WarDimension@users.noreply.github.com>
2020-02-28 19:32:49 +0800

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -2,7 +2,6 @@ function url(){
     var vars = {};
     window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
         vars[key] = value;
-        console.log(key);
     });
     return vars;
 }
