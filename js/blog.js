e47d2662195dea5335fab249724426562ee841b3
WarDimension <49086905+WarDimension@users.noreply.github.com>
2020-02-28 19:29:53 +0800

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -1,15 +1,31 @@
-function url(parameter){
+function url(){
     var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(parameter,key,value) {
+    window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
         vars[key] = value;
+        console.log(key);
     });
     return vars;
 }
 
-if(["experimental", "chart", "tab"].indexOf(url("b")["b"]) >= 0){
+if(["experimental", "chart", "tab"].indexOf(url()["b"]) >= 0){
     var blog = document.getElementsByClassName("blog");
-    blog[0].src = `https://wardimension-${url("b")["b"]}.blogspot.com/`;
-    if(url("p")["p"]!=undefined&&url("p")["p"]!=""){
-        blog[0].src += `${url("p")["p"]}.html`;
+    blog[0].src = `https://wardimension-${url()["b"]}.blogspot.com/`;
+    if(url()["p"]!=undefined&&url()["p"]!=""){
+        blog[0].src += `${url()["p"]}.html`;
     }
 }
+
+window.addEventListener("message", function(e) {
+    console.log(e.data);
+    if (["https://wardimension-experimental.blogspot.com","https://wardimension-chart.blogspot.com","https://wardimension-tab.blogspot.com"].indexOf(e.origin) >= 0){
+        const url = new URL(window.location);
+        var b = e.data[0];
+        var p = "";
+        if(e.data[1] != undefined){
+            p = `&p=${e.data[1]}`;
+        }
+        url.search = `?b=${b}${p}`;
+        console.log(url.search);
+        history.replaceState(null, document.title, url);
+    }
+});
