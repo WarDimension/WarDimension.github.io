14f106b8a57c8ba3df6278c075d43d0a3d6afcd5
WarDimension <49086905+WarDimension@users.noreply.github.com>
2020-02-29 12:01:32 +0800

    666

---
+++ js/666.js
@@ -0,0 +1,47 @@
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: 'HmlSNmbTUw4',
+        playerVars: {
+            'autoplay': 1,
+            'controls': 0,
+            'showinfo': 0,
+            'loop': 1,
+            'rel': 0
+        },
+        /*events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }*/
+    });
+}
+/*
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+var done = false;
+
+function onPlayerStateChange(event) {
+    if (event.data === YT.PlayerState.ENDED) {
+        player.playVideo();
+    }
+}
+
+function stopVideo() {
+    player.stopVideo();
+}*/
+
+const block = document.querySelector(".block");
+
+block.addEventListener("click", (e) => {
+    player.playVideo();
+});
