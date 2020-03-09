afb52251301f77de110ddaba854313a6ebffbeb3
WarDimension <49086905+WarDimension@users.noreply.github.com>
2020-03-09 14:20:19 +0800

    just move it

--- js/song.js
+++ js/song.js
@@ -478,15 +478,3 @@ function updateTimeSlider(){
     currentTimeText.innerHTML = player.getCurrentTime().toString().toHHMMSS();
   }
 }
-
-String.prototype.toHHMMSS = function () {
-  var sec_num = parseInt(this, 10); // don't forget the second param
-  var hours   = Math.floor(sec_num / 3600);
-  var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-  var seconds = sec_num - (hours * 3600) - (minutes * 60);
-
-  if (hours   < 1) {hours   = ""} else {hours = hours+":"}
-  if (minutes < 1) {minutes = "0"}
-  if (seconds < 10) {seconds = "0"+seconds;}
-  return hours+minutes+':'+seconds;
-}
