e1fae071bae293df0d4829683e31f8e9c32f42eb
WarDimension <49086905+WarDimension@users.noreply.github.com>
2020-07-30 18:47:47 +0800

    keyboard

--- project/keyboard/keys.js
+++ project/keyboard/keys.js
@@ -1,34 +1,3 @@
-var shiftKey = 0;
-var highestNote = "C4";
-var highestOctave = 3;
-
-// LOAD SAMPLES
-
-const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-
-var samples = "stage-grand";
-
-function loadSamples(){
-    var audio;
-    var src = "";
-    for(var i = 1; i <= highestOctave; i++){
-        notes.forEach(note => {
-            src = "./notes/" + samples + "/" + note.replace("#", "%23") + i.toString() + ".ogg";
-            audio = new Audio(src);
-            audio.load();
-            audio.remove();
-        });
-    }
-    src = "./notes/" + samples + "/" + highestNote + ".ogg";
-    audio = new Audio(src);
-    audio.load();
-    audio.remove();
-}
-
-loadSamples();
-
-// END LOAD SAMPLES
-
 var audio = [];
 
 function playNote(note){
