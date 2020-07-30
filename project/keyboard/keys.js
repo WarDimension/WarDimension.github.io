219b2a0eaa0d06ca803978ea445b0fc53f7dcbf0
WarDimension <49086905+WarDimension@users.noreply.github.com>
2020-07-30 21:12:31 +0800

    Update keys.js

--- project/keyboard/keys.js
+++ project/keyboard/keys.js
@@ -17,15 +17,11 @@ function loadSamples(){
             src = "./notes/" + samples + "/" + note.replace("#", "%23") + i.toString() + ".ogg";
             audioSamples.push(new Audio(src));
             audioSamples[audioSamples.length - 1].load();
-            audioSamples[audioSamples.length - 1].volume = 0;
-            audioSamples[audioSamples.length - 1].play();
         });
     }
     src = "./notes/" + samples + "/" + highestNote + ".ogg";
     audioSamples.push(new Audio(src));
     audioSamples[audioSamples.length - 1].load();
-    audioSamples[audioSamples.length - 1].volume = 0;
-    audioSamples[audioSamples.length - 1].play();
 
     loadTimer = setInterval(loadTime, 100);
 }
@@ -41,7 +37,7 @@ function loadTime(){
     audioSamples.forEach(audio => {
         if(audio.readyState < 3){
             isReady = false;
-            audio.play();
+            audio.load();
         }
     });
 
