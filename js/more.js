b0aa01ca90c077df9748198164ec2bc2bfcc801b
WarDimension <49086905+WarDimension@users.noreply.github.com>
2020-08-13 01:04:25 +0800

    Update more.js

--- js/more.js
+++ js/more.js
@@ -56,11 +56,11 @@ var games_i = document.getElementsByClassName("games-i");
 var prev_list = document.getElementsByClassName("prev-list");
 var next_list = document.getElementsByClassName("next-list");
 
-var apps_list = ["Ritsu.exe", "Mio.exe", "WarDimension_VirtualDrums", "GH!mania", "WarDimension Official Extension", "osu!play", "Keyboard"];
-var apps_link = ["https://wardimension.github.io/blog?b=experimental&p=2019/02/ritsu.exe", "https://wardimension.github.io/blog?b=experimental&p=2019/02/mio.exe", "https://wardimension.github.io/blog?b=experimental&p=2019/02/virtual-drums", "https://wardimension.github.io/blog?b=experimental&p=2019/02/gh-mania", "https://wardimension.github.io/blog?b=experimental&p=2020/02/wardimension-official-extension", "https://wardimension.github.io/blog?b=experimental&p=2020/04/osu-play", "https://wardimension.github.io/project/keyboard"];
+var apps_list = ["Ritsu.exe", "Mio.exe", "WarDimension_VirtualDrums", "GH!mania", "WarDimension Official Extension", "osu!play", "Keyboard", "List Sorter"];
+var apps_link = ["https://wardimension.github.io/blog?b=experimental&p=2019/02/ritsu.exe", "https://wardimension.github.io/blog?b=experimental&p=2019/02/mio.exe", "https://wardimension.github.io/blog?b=experimental&p=2019/02/virtual-drums", "https://wardimension.github.io/blog?b=experimental&p=2019/02/gh-mania", "https://wardimension.github.io/blog?b=experimental&p=2020/02/wardimension-official-extension", "https://wardimension.github.io/blog?b=experimental&p=2020/04/osu-play", "https://wardimension.github.io/project/keyboard", "https://wardimension.github.io/project/list-sorter"];
 
 var games_list = ["UNO_command"];
-var games_link = ["https://wardimension.github.io/project/uno-command/"];
+var games_link = ["https://wardimension.github.io/project/uno-command"];
 
 list_index = 0;
 
