e1f3768517d355358f30a3d2d567cef582ec0709
WarDimension <49086905+WarDimension@users.noreply.github.com>
2020-10-25 12:02:09 +0800

    𝄞 ⁴⁄₄ ᴅ♪ ᴇ♪ ғ♪ ɢ♪ ᴇ♩ ᴄ♪ ᴅ♪‿𝅝

--- js/global.js
+++ js/global.js
@@ -124,4 +124,4 @@ String.prototype.toHHMMSS = function () {
   return hours+minutes+':'+seconds;
 }
 
-console.log("𝄞 ⁴⁄₄ ᴅ♪ ᴇ♪ ғ♪ ɢ♪ ᴇ♩ ᴄ♪ ᴅ♪‿𝅝");
+console.log("%c𝄞 ⁴⁄₄ ᴅ♪ ᴇ♪ ғ♪ ɢ♪ ᴇ♩ ᴄ♪ ᴅ♪‿𝅝", "font-size: 18px");
