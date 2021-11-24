8a10ab9d2276055eb11b3b9cf715eabea2557aa6
WarDimension <49086905+WarDimension@users.noreply.github.com>
2021-11-24 20:47:26 +0800

    Update global.js

--- js/global.js
+++ js/global.js
@@ -1,3 +1,30 @@
+console.clear();
+
+console.group("FUN (.❛ ᴗ ❛.)");
+  console.log("%c│𝄞 ⁴⁄₄ ᴅ♪ ᴇ♪ ғ♪ ɢ♪ ᴇ♩ ᴄ♪ ᴅ♪│‿𝅝║", "font-size: 16px; margin: 10px 0;");
+
+  var consoleStyle = "padding: 1px 6px; color: #fbfbfb; background: #444;";
+
+  console.groupCollapsed("%c[-GAMES-]", consoleStyle);
+    console.log("%cUNO_command", consoleStyle, "https://wardimension.github.io/project/uno-command");
+    console.log("%cGit-Hero", consoleStyle, "https://wardimension.github.io/project/git-hero");
+  console.groupEnd();
+
+  console.groupCollapsed("%c[-APPS-]", consoleStyle);
+    console.log("%cRitsu.exe", consoleStyle, "https://wardimension.github.io/blog?b=experimental&p=2019/02/ritsu.exe");
+    console.log("%cMio.exe", consoleStyle, "https://wardimension.github.io/blog?b=experimental&p=2019/02/mio.exe");
+    console.log("%cWarDimension_VirtualDrums", consoleStyle, "https://wardimension.github.io/blog?b=experimental&p=2019/02/virtual-drums");
+    console.log("%cGH!mania", consoleStyle, "https://wardimension.github.io/blog?b=experimental&p=2019/02/gh-mania");
+    console.log("%cWarDimension Official Extension", consoleStyle, "https://wardimension.github.io/blog?b=experimental&p=2020/02/wardimension-official-extension");
+    console.log("%cosu!play", consoleStyle, "https://wardimension.github.io/blog?b=experimental&p=2020/04/osu-play");
+    console.log("%cKeyboard", consoleStyle, "https://wardimension.github.io/project/keyboard");
+    console.log("%cList Sorter", consoleStyle, "https://wardimension.github.io/project/list-sorter");
+    console.log("%cCharacter Counter", consoleStyle, "https://wardimension.github.io/project/character-counter");
+  console.groupEnd();
+console.groupEnd();
+
+console.groupCollapsed("%cThe End of the World", "padding: 1px 8px; color: #fff; background: #e2363a; border-radius: 15px;");
+
 function removeHash(){
     const url = new URL(window.location);
     url.hash = "";
@@ -102,31 +129,3 @@ String.prototype.toHHMMSS = function () {
   if (seconds < 10) {seconds = "0"+seconds;}
   return hours+minutes+':'+seconds;
 }
-
-console.clear();
-
-console.group("FUN (.❛ ᴗ ❛.)");
-  console.log("%c│𝄞 ⁴⁄₄ ᴅ♪ ᴇ♪ ғ♪ ɢ♪ ᴇ♩ ᴄ♪ ᴅ♪│‿𝅝║", "font-size: 16px; margin: 10px 0;");
-
-  var consoleStyle = "padding: 1px 6px; color: #fbfbfb; background: #444;";
-
-  console.groupCollapsed("%c[-GAMES-]", consoleStyle);
-    console.log("%cUNO_command", consoleStyle, "https://wardimension.github.io/project/uno-command");
-    console.log("%cGit-Hero", consoleStyle, "https://wardimension.github.io/project/git-hero");
-  console.groupEnd();
-
-  console.groupCollapsed("%c[-APPS-]", consoleStyle);
-    console.log("%cRitsu.exe", consoleStyle, "https://wardimension.github.io/blog?b=experimental&p=2019/02/ritsu.exe");
-    console.log("%cMio.exe", consoleStyle, "https://wardimension.github.io/blog?b=experimental&p=2019/02/mio.exe");
-    console.log("%cWarDimension_VirtualDrums", consoleStyle, "https://wardimension.github.io/blog?b=experimental&p=2019/02/virtual-drums");
-    console.log("%cGH!mania", consoleStyle, "https://wardimension.github.io/blog?b=experimental&p=2019/02/gh-mania");
-    console.log("%cWarDimension Official Extension", consoleStyle, "https://wardimension.github.io/blog?b=experimental&p=2020/02/wardimension-official-extension");
-    console.log("%cosu!play", consoleStyle, "https://wardimension.github.io/blog?b=experimental&p=2020/04/osu-play");
-    console.log("%cKeyboard", consoleStyle, "https://wardimension.github.io/project/keyboard");
-    console.log("%cList Sorter", consoleStyle, "https://wardimension.github.io/project/list-sorter");
-    console.log("%cCharacter Counter", consoleStyle, "https://wardimension.github.io/project/character-counter");
-  console.groupEnd();
-console.groupEnd();
-
-console.groupCollapsed("%cThe End of the World", "padding: 1px 8px; color: #fff; background: #e2363a; border-radius: 15px;");
-
