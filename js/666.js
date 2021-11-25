53ccf1f2e7830cd39132f09d6377e4a57960bc5a
WarDimension <49086905+WarDimension@users.noreply.github.com>
2021-11-26 02:57:03 +0800

    Update 666.js

--- js/666.js
+++ js/666.js
@@ -74,7 +74,7 @@ window.addEventListener("keydown", (e) => {
   else if(e.key == "Enter" && active.className == "skip"){
     effect(true);
   }
-  else if(e.key == "Enter" && doYouWishToAscend.toLocaleLowerCase().includes("ascend")){
+  else if(e.key == "Enter" && active.className == "sixsixsix" && doYouWishToAscend.toLocaleLowerCase().includes(/ascend$/)){
     localStorage.setItem("ascend1", "true");
     window.open("https://wardimension.github.io/👁","_top");
   }
