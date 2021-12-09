e8a48e8b828d40bfd4133dfb7035e17a8d561017
WarDimension <49086905+WarDimension@users.noreply.github.com>
2021-12-09 23:15:49 +0800

    fix the original : cover shuffle probability

--- js/song.js
+++ js/song.js
@@ -318,6 +318,13 @@ function shuffleSong(push = true){
   var title;
   var trackIndex;
 
+  var randomType = Math.floor(Math.random() * 100);
+
+  var totalSongs = songsData.length + coversData.length;
+  var coversPercentage = Math.round(coversData.length / totalSongs * 100);
+
+  randomType < coversPercentage ? randomType = 1 : randomType = 0;
+
   if(repeat == "repeat album") currentTrack.type == "original" ? randomType = 0 : randomType = 1;
 
   switch(randomType){
