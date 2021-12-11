afcef4109c8c2a260dcc232d0506c5a931d0b245
WarDimension <49086905+WarDimension@users.noreply.github.com>
2021-12-12 02:29:31 +0800

    set min width to 600px, remove @media query < 600px, fix skip for lower screen width

--- js/global.js
+++ js/global.js
@@ -56,13 +56,13 @@ function url(){
 }
 
 var mvp = document.getElementById("vp");
-if(screen.width < 360) {
-  mvp.setAttribute("content","width=360, user-scalable=no");
+if(screen.width < 600) {
+  mvp.setAttribute("content","width=600, user-scalable=no");
 }
 
 window.addEventListener("resize", (e) => {
-  if(screen.width < 360) {
-    mvp.setAttribute("content","width=360, user-scalable=no");
+  if(screen.width < 600) {
+    mvp.setAttribute("content","width=600, user-scalable=no");
   }else{
     mvp.setAttribute("content","width=device-width, initial-scale=1.0, user-scalable=no");
   }
