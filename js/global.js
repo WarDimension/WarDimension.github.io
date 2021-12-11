ed28b098f28db8fc57ea0f559d9b1c5cc5809a6f
WarDimension <49086905+WarDimension@users.noreply.github.com>
2021-12-12 02:56:05 +0800

    change min width to 500px

--- js/global.js
+++ js/global.js
@@ -56,13 +56,13 @@ function url(){
 }
 
 var mvp = document.getElementById("vp");
-if(screen.width < 600) {
-  mvp.setAttribute("content","width=600, user-scalable=no");
+if(screen.width < 500) {
+  mvp.setAttribute("content","width=500, user-scalable=no");
 }
 
 window.addEventListener("resize", (e) => {
-  if(screen.width < 600) {
-    mvp.setAttribute("content","width=600, user-scalable=no");
+  if(screen.width < 500) {
+    mvp.setAttribute("content","width=500, user-scalable=no");
   }else{
     mvp.setAttribute("content","width=device-width, initial-scale=1.0, user-scalable=no");
   }
