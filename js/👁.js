17377f8a5718b83c3ba6ef40a584df8f58258841
WarDimension <49086905+WarDimension@users.noreply.github.com>
2022-06-16 15:44:25 +0800

    👁

---
+++ js/👁.js
@@ -0,0 +1,28 @@
+let glyphs = document.querySelector(".glyphs");
+
+let centerX = window.innerWidth / 2;
+let centerY = window.innerHeight / 2;
+
+glyphs.style.setProperty("--x", centerX + "px");
+glyphs.style.setProperty("--y", centerY + "px");
+
+function setPosition(x, y){
+    glyphs.style.setProperty("--x", x + "px");
+    glyphs.style.setProperty("--y", y + "px");
+
+}
+
+document.addEventListener("mousemove", (e) => {
+    let x = e.pageX;
+    let y = e.pageY;
+
+    setPosition(x, y);
+});
+
+document.addEventListener("touchmove", (e) => {
+    let touch = e.touches[0] || e.changedTouches[0];
+    let x = touch.pageX;
+    let y = touch.pageY;
+
+    setPosition(x, y);
+});
