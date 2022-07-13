09574beeba791f048878cda0b858814f4cf8bc36
WarDimension <49086905+WarDimension@users.noreply.github.com>
2022-07-13 22:03:31 +0800

    :O

--- collections/js/watch-order.js
+++ collections/js/watch-order.js
@@ -1,3 +1,23 @@
+/* TEMPLATE
+
+    {
+        "name": "",
+        "tags": "",
+        "list": [
+            {
+                "title": "",
+                "AniList": "",
+                "MyAnimeList": ""
+            },
+            {
+                "title": "",
+                "AniList": "",
+                "MyAnimeList": ""
+            }
+        ]
+    },
+*/
+
 let series = [
     {
         "name": "180-byou de Kimi no Mimi wo Shiawase ni Dekiru ka?",
@@ -5,7 +25,8 @@ let series = [
         "list": [
             {
                 "title": "180-byou de Kimi no Mimi wo Shiawase ni Dekiru ka?",
-                "link": "https://anilist.co/anime/133411/180byou-de-Kimi-no-Mimi-wo-Shiawase-ni-Dekiru-ka/"
+                "AniList": "https://anilist.co/anime/133411/180byou-de-Kimi-no-Mimi-wo-Shiawase-ni-Dekiru-ka/",
+                "MyAnimeList": ""
             }
         ]
     },
@@ -15,11 +36,12 @@ let series = [
         "list": [
             {
                 "title": "3-gatsu no Lion",
-                "link": "https://anilist.co/anime/21366/3gatsu-no-Lion/"
+                "AniList": "https://anilist.co/anime/21366/3gatsu-no-Lion/",
+                "MyAnimeList": "https://myanimelist.net/anime/31646/3-gatsu_no_Lion"
             },
             {
                 "title": "3-gatsu no Lion 2",
-                "link": "https://anilist.co/anime/98478/3gatsu-no-Lion-2/"
+                "AniList": "https://anilist.co/anime/98478/3gatsu-no-Lion-2/"
             }
         ]
     },
@@ -29,11 +51,11 @@ let series = [
         "list": [
             {
                 "title": "3D Kanojo: Real Girl",
-                "link": "https://anilist.co/anime/100526/3D-Kanojo-Real-Girl/"
+                "AniList": "https://anilist.co/anime/100526/3D-Kanojo-Real-Girl/"
             },
             {
                 "title": "3D Kanojo: Real Girl 2",
-                "link": "https://anilist.co/anime/102882/3D-Kanojo-Real-Girl-2/"
+                "AniList": "https://anilist.co/anime/102882/3D-Kanojo-Real-Girl-2/"
             }
         ]
     },
@@ -43,7 +65,7 @@ let series = [
         "list": [
             {
                 "title": "A.I.C.O. -Incarnation-",
-                "link": "https://anilist.co/anime/99725/AICO-Incarnation/"
+                "AniList": "https://anilist.co/anime/99725/AICO-Incarnation/"
             }
         ]
     },
@@ -53,11 +75,11 @@ let series = [
         "list": [
             {
                 "title": "Adachi to Shimamura",
-                "link": "https://anilist.co/anime/109287/Adachi-to-Shimamura/"
+                "AniList": "https://anilist.co/anime/109287/Adachi-to-Shimamura/"
             },
             {
                 "title": "Adachi to Shimamura Mini Anime",
-                "link": "https://anilist.co/anime/127465/Adachi-to-Shimamura-Mini-Anime/"
+                "AniList": "https://anilist.co/anime/127465/Adachi-to-Shimamura-Mini-Anime/"
             }
         ]
     },
@@ -67,7 +89,7 @@ let series = [
         "list": [
             {
                 "title": "Aho-Girl",
-                "link": "https://anilist.co/anime/98251/AhoGirl/"
+                "AniList": "https://anilist.co/anime/98251/AhoGirl/"
             }
         ]
     },
@@ -76,7 +98,7 @@ let series = [
         "list": [
             {
                 "title": "citrus",
-                "link": "https://anilist.co/anime/97832/citrus/"
+                "AniList": "https://anilist.co/anime/97832/citrus/"
             }
         ]
     },
@@ -86,39 +108,39 @@ let series = [
         "list": [
             {
                 "title": "Higurashi no Naku Koro ni",
-                "link": "https://anilist.co/anime/934/Higurashi-no-Naku-Koro-ni/"
+                "AniList": "https://anilist.co/anime/934/Higurashi-no-Naku-Koro-ni/"
             },
             {
                 "title": "Higurashi no Naku Koro ni: Nekogoroshi-hen",
-                "link": "https://anilist.co/anime/2899/Higurashi-no-Naku-Koro-ni-Nekogoroshihen/"
+                "AniList": "https://anilist.co/anime/2899/Higurashi-no-Naku-Koro-ni-Nekogoroshihen/"
             },
             {
                 "title": "Higurashi no Naku Koro ni Kaku: Outbreak",
-                "link": "https://anilist.co/anime/16700/Higurashi-no-Naku-Koro-ni-Kaku-Outbreak/"
+                "AniList": "https://anilist.co/anime/16700/Higurashi-no-Naku-Koro-ni-Kaku-Outbreak/"
             },
             {
                 "title": "Higurashi no Naku Koro ni Kira",
-                "link": "https://anilist.co/anime/10491/Higurashi-no-Naku-Koro-ni-Kira/"
+                "AniList": "https://anilist.co/anime/10491/Higurashi-no-Naku-Koro-ni-Kira/"
             },
             {
                 "title": "Higurashi no Naku Koro ni Kai",
-                "link": "https://anilist.co/anime/1889/Higurashi-no-Naku-Koro-ni-Kai/"
+                "AniList": "https://anilist.co/anime/1889/Higurashi-no-Naku-Koro-ni-Kai/"
             },
             {
                 "title": "Ura☆Higu",
-                "link": "https://anilist.co/anime/6064/UraHigu/"
+                "AniList": "https://anilist.co/anime/6064/UraHigu/"
             },
             {
                 "title": "Higurashi no Naku Koro ni Rei",
-                "link": "https://anilist.co/anime/3652/Higurashi-no-Naku-Koro-ni-Rei/"
+                "AniList": "https://anilist.co/anime/3652/Higurashi-no-Naku-Koro-ni-Rei/"
             },
             {
                 "title": "Higurashi no Naku Koro ni Gou",
-                "link": "https://anilist.co/anime/114446/Higurashi-no-Naku-Koro-ni-Gou/"
+                "AniList": "https://anilist.co/anime/114446/Higurashi-no-Naku-Koro-ni-Gou/"
             },
             {
                 "title": "Higurashi no Naku Koro ni Sotsu",
-                "link": "https://anilist.co/anime/131149/Higurashi-no-Naku-Koro-ni-Sotsu/"
+                "AniList": "https://anilist.co/anime/131149/Higurashi-no-Naku-Koro-ni-Sotsu/"
             }
         ]
     },
@@ -128,7 +150,7 @@ let series = [
         "list": [
             {
                 "title": "Yagate Kimi ni Naru",
-                "link": "https://anilist.co/anime/101573/Yagate-Kimi-ni-Naru/"
+                "AniList": "https://anilist.co/anime/101573/Yagate-Kimi-ni-Naru/"
             }
         ]
     },
@@ -138,27 +160,27 @@ let series = [
         "list": [
             {
                 "title": "Yahari Ore no Seishun Love Come wa Machigatteiru.",
-                "link": "https://anilist.co/anime/14813/Yahari-Ore-no-Seishun-Love-Come-wa-Machigatteiru/"
+                "AniList": "https://anilist.co/anime/14813/Yahari-Ore-no-Seishun-Love-Come-wa-Machigatteiru/"
             },
             {
                 "title": "Yahari Ore no Seishun Love Come wa Machigatteiru.: Kochira to Shite mo Karera Kanojora no Yukusue ni Sachi Ookaran Koto wo Negawazaru wo Enai.",
-                "link": "https://anilist.co/anime/18753/Yahari-Ore-no-Seishun-Love-Come-wa-Machigatteiru-Kochira-to-Shite-mo-Karera-Kanojora-no-Yukusue-ni-Sachi-Ookaran-Koto-wo-Negawazaru-wo-Enai/"
+                "AniList": "https://anilist.co/anime/18753/Yahari-Ore-no-Seishun-Love-Come-wa-Machigatteiru-Kochira-to-Shite-mo-Karera-Kanojora-no-Yukusue-ni-Sachi-Ookaran-Koto-wo-Negawazaru-wo-Enai/"
             },
             {
                 "title": "Yahari Ore no Seishun Love Come wa Machigatteiru. Zoku",
-                "link": "https://anilist.co/anime/20698/Yahari-Ore-no-Seishun-Love-Come-wa-Machigatteiru-Zoku/"
+                "AniList": "https://anilist.co/anime/20698/Yahari-Ore-no-Seishun-Love-Come-wa-Machigatteiru-Zoku/"
             },
             {
                 "title": "Yahari Ore no Seishun Love Come wa Machigatteiru. Zoku: Kitto, Onnanoko wa Osatou to Spice to Suteki na Nanika de Dekiteiru",
-                "link": "https://anilist.co/anime/21769/Yahari-Ore-no-Seishun-Love-Come-wa-Machigatteiru-Zoku-Kitto-Onnanoko-wa-Osatou-to-Spice-to-Suteki-na-Nanika-de-Dekiteiru/"
+                "AniList": "https://anilist.co/anime/21769/Yahari-Ore-no-Seishun-Love-Come-wa-Machigatteiru-Zoku-Kitto-Onnanoko-wa-Osatou-to-Spice-to-Suteki-na-Nanika-de-Dekiteiru/"
             },
             {
                 "title": "Yahari Ore no Seishun Love Come wa Machigatteiru. Kan",
-                "link": "https://anilist.co/anime/108489/Yahari-Ore-no-Seishun-Love-Come-wa-Machigatteiru-Kan/"
+                "AniList": "https://anilist.co/anime/108489/Yahari-Ore-no-Seishun-Love-Come-wa-Machigatteiru-Kan/"
             },
             {
                 "title": "Yahari Ore no Seishun Love Come wa Machigatteiru. Kan OVA",
-                "link": "https://anilist.co/anime/128643/Yahari-Ore-no-Seishun-Love-Come-wa-Machigatteiru-Kan-OVA/"
+                "AniList": "https://anilist.co/anime/128643/Yahari-Ore-no-Seishun-Love-Come-wa-Machigatteiru-Kan-OVA/"
             }
         ]
     },
@@ -168,54 +190,59 @@ let series = [
         "list": [
             {
                 "title": "Yuru Yuri",
-                "link": "https://anilist.co/anime/10495/Yuru-Yuri/"
+                "AniList": "https://anilist.co/anime/10495/Yuru-Yuri/"
             },
             {
                 "title": "Yuru Yuri: Doushite☆Tomaranai☆Tokimeki☆Doki Doki☆Paradox☆Eternal",
-                "link": "https://anilist.co/anime/12665/Yuru-Yuri-DoushiteTomaranaiTokimekiDoki-DokiParadoxEternal/"
+                "AniList": "https://anilist.co/anime/12665/Yuru-Yuri-DoushiteTomaranaiTokimekiDoki-DokiParadoxEternal/"
             },
             {
                 "title": "Yuru Yuri♪♪",
-                "link": "https://anilist.co/anime/12403/Yuru-Yuri/"
+                "AniList": "https://anilist.co/anime/12403/Yuru-Yuri/"
             },
             {
                 "title": "Yuru Yuri Nachuyachumi!",
-                "link": "https://anilist.co/anime/20625/Yuru-Yuri-Nachuyachumi/"
+                "AniList": "https://anilist.co/anime/20625/Yuru-Yuri-Nachuyachumi/"
             },
             {
                 "title": "Yuru Yuri Nachuyachumi!+",
-                "link": "https://anilist.co/anime/21214/Yuru-Yuri-Nachuyachumi/"
+                "AniList": "https://anilist.co/anime/21214/Yuru-Yuri-Nachuyachumi/"
             },
             {
                 "title": "Yuru Yuri San☆Hai!",
-                "link": "https://anilist.co/anime/21088/Yuru-Yuri-SanHai/"
+                "AniList": "https://anilist.co/anime/21088/Yuru-Yuri-SanHai/"
             },
             {
                 "title": "Yuru Yuri San☆Hai! Happy Go Lucky",
-                "link": "https://www.youtube.com/playlist?list=PLLPYMaP0tgFJxb0NNM_Fn5V_jzKVTHf9j"
+                "YouTube": "https://www.youtube.com/playlist?list=PLLPYMaP0tgFJxb0NNM_Fn5V_jzKVTHf9j"
             },
             {
                 "title": "Yuru Yuri,",
-                "link": "https://anilist.co/anime/102090/Yuru-Yuri/"
+                "AniList": "https://anilist.co/anime/102090/Yuru-Yuri/"
             },
             {
                 "title": "Mini Yuri",
-                "link": "https://anilist.co/anime/110382/Mini-Yuri/"
+                "AniList": "https://anilist.co/anime/110382/Mini-Yuri/"
             }
         ]
     }
 ];
 let search = document.querySelector(".search");
+let database = document.querySelector(".database");
+let hideCheckbox = document.querySelector(".hide");
 
 function showList(){
     let content = "";
 
     series.forEach(show => {
-        if(search.value == null || JSON.stringify(show).match(new RegExp(search.value, "mi"))){
+        if((search.value == null || JSON.stringify(show).match(new RegExp(search.value, "mi"))) && (JSON.stringify(show).match(new RegExp(database.value, "m")) || !hideCheckbox.checked)){
             content += `<p>${show.name}</p>`;
             content += "<ol>";
             show.list.forEach(item => {
-                content += `<a href="${item.link}" target="_blank"><li>${item.title}</li></a>`;
+                if(item[database.value] != undefined || !hideCheckbox.checked){
+                    let link = item[database.value] != undefined? item[database.value] : item[Object.keys(item)[1]];
+                    content += `<a href="${link}" target="_blank"><li>${item.title}</li></a>`;
+                }
             })
             content += "</ol>";
         }
