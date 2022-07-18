f7834e3e6fae26db14e8f7cbf6d08cb8afa1bdae
WarDimension <49086905+WarDimension@users.noreply.github.com>
2022-07-18 18:02:02 +0800

    hmm

---
+++ collections/js/anime.js
@@ -0,0 +1,426 @@
+const series = [
+    {
+        "name": "180-byou de Kimi no Mimi wo Shiawase ni Dekiru ka?",
+        "tags": ["180秒で君の耳を幸せにできるか？","Can I Make Your Ears Happy in 180 Seconds?"],
+        "list": [
+            {
+                "title": "180-byou de Kimi no Mimi wo Shiawase ni Dekiru ka?",
+                "AniList": "https://anilist.co/anime/133411/180byou-de-Kimi-no-Mimi-wo-Shiawase-ni-Dekiru-ka/",
+                "MyAnimeList": "https://myanimelist.net/anime/48858/180_Byou_de_Kimi_no_Mimi_wo_Shiawase_ni_Dekiru_ka"
+            }
+        ]
+    },
+    {
+        "name": "3-gatsu no Lion",
+        "tags": ["３月のライオン","Sangatsu no Lion","San-gatsu no Lion","March comes in like a lion"],
+        "list": [
+            {
+                "title": "3-gatsu no Lion",
+                "AniList": "https://anilist.co/anime/21366/3gatsu-no-Lion/",
+                "MyAnimeList": "https://myanimelist.net/anime/31646/3-gatsu_no_Lion"
+            },
+            {
+                "title": "3-gatsu no Lion 2",
+                "AniList": "https://anilist.co/anime/98478/3gatsu-no-Lion-2/",
+                "MyAnimeList": "https://myanimelist.net/anime/35180/3-gatsu_no_Lion_2nd_Season"
+            }
+        ]
+    },
+    {
+        "name": "3D Kanojo: Real Girl",
+        "tags": ["3D彼女 リアルガール","3D Girlfriend"],
+        "list": [
+            {
+                "title": "3D Kanojo: Real Girl",
+                "AniList": "https://anilist.co/anime/100526/3D-Kanojo-Real-Girl/",
+                "MyAnimeList": "https://myanimelist.net/anime/36793/3D_Kanojo__Real_Girl"
+            },
+            {
+                "title": "3D Kanojo: Real Girl 2",
+                "AniList": "https://anilist.co/anime/102882/3D-Kanojo-Real-Girl-2/",
+                "MyAnimeList": "https://myanimelist.net/anime/37956/3D_Kanojo__Real_Girl_2nd_Season"
+            }
+        ]
+    },
+    {
+        "name": "A.I.C.O. -Incarnation-",
+        "tags": ["アイコ","AICO"],
+        "list": [
+            {
+                "title": "A.I.C.O. -Incarnation-",
+                "AniList": "https://anilist.co/anime/99725/AICO-Incarnation/",
+                "MyAnimeList": "https://myanimelist.net/anime/36039/AICO__Incarnation"
+            }
+        ]
+    },
+    {
+        "name": "Adachi to Shimamura",
+        "tags": ["安達としまむら","AdaShima","Adachi and Shimamura"],
+        "list": [
+            {
+                "title": "Adachi to Shimamura",
+                "AniList": "https://anilist.co/anime/109287/Adachi-to-Shimamura/",
+                "MyAnimeList": "https://myanimelist.net/anime/39790/Adachi_to_Shimamura"
+            },
+            {
+                "title": "Adachi to Shimamura Mini Anime",
+                "AniList": "https://anilist.co/anime/127465/Adachi-to-Shimamura-Mini-Anime/",
+                "MyAnimeList": "https://myanimelist.net/anime/44590/Adachi_to_Shimamura_Mini_Anime"
+            }
+        ]
+    },
+    {
+        "name": "Aho-Girl",
+        "tags": ["アホガール","Ahogaru: Clueless Girl"],
+        "list": [
+            {
+                "title": "Aho-Girl",
+                "AniList": "https://anilist.co/anime/98251/AhoGirl/",
+                "MyAnimeList": "https://myanimelist.net/anime/34881/Aho_Girl"
+            }
+        ]
+    },
+    {
+        "name": "Ai no Utagoe wo Kikasete",
+        "tags": ["アイの歌声を聴かせて","Sing a Bit of Harmony"],
+        "list": [
+            {
+                "title": "Ai no Utagoe wo Kikasete",
+                "AniList": "https://anilist.co/anime/123899/Ai-no-Utagoe-wo-Kikasete/",
+                "MyAnimeList": "https://myanimelist.net/anime/42847/Ai_no_Utagoe_wo_Kikasete"
+            }
+        ]
+    },
+    {
+        "name": "AIURA",
+        "tags": ["あいうら"],
+        "list": [
+            {
+                "title": "AIURA",
+                "AniList": "https://anilist.co/anime/17082/Aiura/",
+                "MyAnimeList": "https://myanimelist.net/anime/17082/Aiura"
+            }
+        ]
+    },
+    {
+        "name": "Ajin",
+        "tags": ["亜人"],
+        "list": [
+            {
+                "title": "Ajin",
+                "AniList": "https://anilist.co/anime/21341/Ajin/",
+                "MyAnimeList": "https://myanimelist.net/anime/31580/Ajin"
+            },
+            {
+                "title": "Ajin OVA",
+                "AniListTitle": "Ajin OVA 1-2",
+                "AniList": "https://anilist.co/anime/21487/Ajin-OVA/",
+                "MyAnimeList": "https://myanimelist.net/anime/32015/Ajin_OVA"
+            },
+            {
+                "title": "Ajin 2",
+                "AniList": "https://anilist.co/anime/21799/Ajin-2/",
+                "MyAnimeList": "https://myanimelist.net/anime/33253/Ajin_Part_2"
+            },
+            {
+                "title": "Ajin 2 OVA",
+                "AniListTitle": "Ajin OVA 3",
+                "AniList": "https://anilist.co/anime/21487/Ajin-OVA/",
+                "MyAnimeList": "https://myanimelist.net/anime/36625/Ajin_Part_2_OVA"
+            }
+        ],
+        "alt": [
+            {
+                "title": "Ajin: Shoudou",
+                "AniList": "https://anilist.co/anime/21212/Ajin-Shoudou/",
+                "MyAnimeList": "https://myanimelist.net/anime/30868/Ajin_Part_1__Shoudou"
+            },
+            {
+                "title": "Ajin: Shoutotsu",
+                "AniList": "https://anilist.co/anime/21505/Ajin-Shoutotsu/",
+                "MyAnimeList": "https://myanimelist.net/anime/30869/Ajin_Part_2__Shoutotsu"
+            },
+            {
+                "title": "Ajin: Shougeki",
+                "AniList": "https://anilist.co/anime/21681/Ajin-Shougeki/",
+                "MyAnimeList": "https://myanimelist.net/anime/30870/Ajin_Part_3__Shougeki"
+            }
+        ]
+    },
+    {
+        "name": "Akagami no Shirayuki-hime",
+        "tags": ["赤髪の白雪姫","Snow White with the Red Hair"],
+        "list": [
+            {
+                "title": "Akagami no Shirayuki-hime",
+                "AniList": "https://anilist.co/anime/21058/Akagami-no-Shirayukihime/",
+                "MyAnimeList": "https://myanimelist.net/anime/30123/Akagami_no_Shirayuki-hime"
+            },
+            {
+                "title": "Akagami no Shirayuki-hime: Nandemonai Takaramono, Kono Page",
+                "AniList": "https://anilist.co/anime/21313/Akagami-no-Shirayukihime-Nandemonai-Takaramono-Kono-Page/",
+                "MyAnimeList": "https://myanimelist.net/anime/31483/Akagami_no_Shirayuki-hime__Nandemonai_Takaramono_Kono_Page"
+            },
+            {
+                "title": "Akagami no Shirayuki-hime 2",
+                "AniList": "https://anilist.co/anime/21258/Akagami-no-Shirayukihime-2/",
+                "MyAnimeList": "https://myanimelist.net/anime/31173/Akagami_no_Shirayuki-hime_2nd_Season"
+            }
+        ]
+    },
+    {
+        "name": "Akebi-chan no Sailor Fuku",
+        "tags": ["明日ちゃんのセーラー服","Akebi's Sailor Uniform"],
+        "list": [
+            {
+                "title": "Akebi-chan no Sailor Fuku",
+                "AniList": "https://anilist.co/anime/131548/Akebichan-no-Sailor-Fuku/",
+                "MyAnimeList": "https://myanimelist.net/anime/48553/Akebi-chan_no_Sailor-fuku"
+            }
+        ]
+    },
+    {
+        "name": "Aki no Kanade",
+        "tags": ["アキの奏で"],
+        "list": [
+            {
+                "title": "Aki no Kanade",
+                "AniList": "https://anilist.co/anime/21069/Aki-no-Kanade/",
+                "MyAnimeList": "https://myanimelist.net/anime/29513/Aki_no_Kanade"
+            }
+        ]
+    },
+    {
+        "name": "Akkun to Kanojo",
+        "tags": ["あっくんとカノジョ","Akkun and His Girlfriend","My Sweet Tyrant"],
+        "list": [
+            {
+                "title": "Akkun to Kanojo",
+                "AniList": "https://anilist.co/anime/100645/Akkun-to-Kanojo/",
+                "MyAnimeList": "https://myanimelist.net/anime/36864/Akkun_to_Kanojo"
+            }
+        ]
+    },
+    {
+        "name": "Aku no Hana",
+        "tags": ["惡の華","Flowers of Evil"],
+        "list": [
+            {
+                "title": "Aku no Hana",
+                "AniList": "https://anilist.co/anime/16201/Aku-no-Hana/",
+                "MyAnimeList": "https://myanimelist.net/anime/16201/Aku_no_Hana"
+            }
+        ]
+    },
+    {
+        "name": "Akuma no Riddle",
+        "tags": ["悪魔のリドル","Riddle Story of Devil"],
+        "list": [
+            {
+                "title": "Akuma no Riddle",
+                "AniList": "https://anilist.co/anime/19429/Akuma-no-Riddle/",
+                "MyAnimeList": "https://myanimelist.net/anime/19429/Akuma_no_Riddle"
+            },
+            {
+                "title": "Akuma no Riddle: Shousha wa Dare? Nukiuchi Test",
+                "AniList": "https://anilist.co/anime/20926/Akuma-no-Riddle-Special/",
+                "MyAnimeList": "https://myanimelist.net/anime/24751/Akuma_no_Riddle__Shousha_wa_Dare_Nukiuchi_Test"
+            }
+        ]
+    },
+    {
+        "name": "Amaama to Inazuma",
+        "tags": ["甘々と稲妻","Sweetness & Lightning"],
+        "list": [
+            {
+                "title": "Amaama to Inazuma",
+                "AniList": "https://anilist.co/anime/21659/Amaama-to-Inazuma/",
+                "MyAnimeList": "https://myanimelist.net/anime/32828/Amaama_to_Inazuma"
+            }
+        ]
+    },
+    {
+        "name": "Amanchu!",
+        "tags": ["あまんちゅ！"],
+        "list": [
+            {
+                "title": "Amanchu!",
+                "AniList": "https://anilist.co/anime/21406/Amanchu/",
+                "MyAnimeList": "https://myanimelist.net/anime/31771/Amanchu"
+            },
+            {
+                "title": "Amanchu!: Upyopyo Dive Tsukkome! Umi no Sekai!",
+                "AniList": "https://anilist.co/anime/110578/Amanchu-Upyopyo-Dive-Tsukkome-Umi-no-Sekai/",
+                "MyAnimeList": "https://myanimelist.net/anime/39709/Amanchu__Upyopyo_Dive_Tsukkome_Umi_no_Sekai"
+            },
+            {
+                "title": "Amanchu! Yakusoku no Natsu to Atarashii Omoide no Koto",
+                "AniList": "https://anilist.co/anime/98536/Amanchu-Yakusoku-no-Natsu-to-Atarashii-Omoide-no-Koto/",
+                "MyAnimeList": "https://myanimelist.net/anime/33818/Amanchu__Yakusoku_no_Natsu_to_Atarashii_Omoide_no_Koto"
+            },
+            {
+                "title": "Amanchu! Advance",
+                "AniList": "https://anilist.co/anime/100532/Amanchu-Advance/",
+                "MyAnimeList": "https://myanimelist.net/anime/36800/Amanchu_Advance"
+            }
+        ]
+    },
+    {
+        "name": "citrus",
+        "list": [
+            {
+                "title": "citrus",
+                "AniList": "https://anilist.co/anime/97832/citrus/",
+                "MyAnimeList": "https://myanimelist.net/anime/34382/Citrus"
+            }
+        ]
+    },
+    {
+        "name": "Higurashi no Naku Koro ni",
+        "tags": ["ひぐらしのなく頃に","When They Cry","うら☆ひぐ","Ura Higu"],
+        "list": [
+            {
+                "title": "Higurashi no Naku Koro ni",
+                "AniList": "https://anilist.co/anime/934/Higurashi-no-Naku-Koro-ni/",
+                "MyAnimeList": "https://myanimelist.net/anime/934/Higurashi_no_Naku_Koro_ni"
+            },
+            {
+                "title": "Higurashi no Naku Koro ni: Nekogoroshi-hen",
+                "AniList": "https://anilist.co/anime/2899/Higurashi-no-Naku-Koro-ni-Nekogoroshihen/",
+                "MyAnimeList": "https://myanimelist.net/anime/2899/Higurashi_no_Naku_Koro_ni_Special__Nekogoroshi-hen"
+            },
+            {
+                "title": "Higurashi no Naku Koro ni Kaku: Outbreak",
+                "AniList": "https://anilist.co/anime/16700/Higurashi-no-Naku-Koro-ni-Kaku-Outbreak/",
+                "MyAnimeList": "https://myanimelist.net/anime/16700/Higurashi_no_Naku_Koro_ni_Kaku__Outbreak"
+            },
+            {
+                "title": "Higurashi no Naku Koro ni Kira",
+                "AniList": "https://anilist.co/anime/10491/Higurashi-no-Naku-Koro-ni-Kira/",
+                "MyAnimeList": "https://myanimelist.net/anime/10491/Higurashi_no_Naku_Koro_ni_Kira"
+            },
+            {
+                "title": "Higurashi no Naku Koro ni Kai",
+                "AniList": "https://anilist.co/anime/1889/Higurashi-no-Naku-Koro-ni-Kai/",
+                "MyAnimeList": "https://myanimelist.net/anime/1889/Higurashi_no_Naku_Koro_ni_Kai"
+            },
+            {
+                "title": "Ura☆Higu",
+                "AniList": "https://anilist.co/anime/6064/UraHigu/",
+                "MyAnimeList": "https://myanimelist.net/anime/6064/Higurashi_no_Naku_Koro_ni_Kai_Specials"
+            },
+            {
+                "title": "Higurashi no Naku Koro ni Rei",
+                "AniList": "https://anilist.co/anime/3652/Higurashi-no-Naku-Koro-ni-Rei/",
+                "MyAnimeList": "https://myanimelist.net/anime/3652/Higurashi_no_Naku_Koro_ni_Rei"
+            },
+            {
+                "title": "Higurashi no Naku Koro ni Gou",
+                "AniList": "https://anilist.co/anime/114446/Higurashi-no-Naku-Koro-ni-Gou/",
+                "MyAnimeList": "https://myanimelist.net/anime/41006/Higurashi_no_Naku_Koro_ni_Gou"
+            },
+            {
+                "title": "Higurashi no Naku Koro ni Sotsu",
+                "AniList": "https://anilist.co/anime/131149/Higurashi-no-Naku-Koro-ni-Sotsu/",
+                "MyAnimeList": "https://myanimelist.net/anime/48488/Higurashi_no_Naku_Koro_ni_Sotsu"
+            }
+        ]
+    },
+    {
+        "name": "Yagate Kimi ni Naru",
+        "tags": ["やがて君になる","Bloom Into You","YagaKimi"],
+        "list": [
+            {
+                "title": "Yagate Kimi ni Naru",
+                "AniList": "https://anilist.co/anime/101573/Yagate-Kimi-ni-Naru/",
+                "MyAnimeList": "https://myanimelist.net/anime/37786/Yagate_Kimi_ni_Naru"
+            }
+        ]
+    },
+    {
+        "name": "Yahari Ore no Seishun Love Come wa Machigatteiru.",
+        "tags": ["やはり俺の青春ラブコメはまちがっている。","俺ガイル","Oregairu","My Teen Romantic Comedy SNAFU","My youth romantic comedy is wrong as I expected."],
+        "list": [
+            {
+                "title": "Yahari Ore no Seishun Love Come wa Machigatteiru.",
+                "AniList": "https://anilist.co/anime/14813/Yahari-Ore-no-Seishun-Love-Come-wa-Machigatteiru/",
+                "MyAnimeList": "https://myanimelist.net/anime/14813/Yahari_Ore_no_Seishun_Love_Comedy_wa_Machigatteiru"
+            },
+            {
+                "title": "Yahari Ore no Seishun Love Come wa Machigatteiru.: Kochira to Shite mo Karera Kanojora no Yukusue ni Sachi Ookaran Koto wo Negawazaru wo Enai.",
+                "AniList": "https://anilist.co/anime/18753/Yahari-Ore-no-Seishun-Love-Come-wa-Machigatteiru-Kochira-to-Shite-mo-Karera-Kanojora-no-Yukusue-ni-Sachi-Ookaran-Koto-wo-Negawazaru-wo-Enai/",
+                "MyAnimeList": "https://myanimelist.net/anime/18753/Yahari_Ore_no_Seishun_Love_Comedy_wa_Machigatteiru_OVA"
+            },
+            {
+                "title": "Yahari Ore no Seishun Love Come wa Machigatteiru. Zoku",
+                "AniList": "https://anilist.co/anime/20698/Yahari-Ore-no-Seishun-Love-Come-wa-Machigatteiru-Zoku/",
+                "MyAnimeList": "https://myanimelist.net/anime/23847/Yahari_Ore_no_Seishun_Love_Comedy_wa_Machigatteiru_Zoku"
+            },
+            {
+                "title": "Yahari Ore no Seishun Love Come wa Machigatteiru. Zoku: Kitto, Onnanoko wa Osatou to Spice to Suteki na Nanika de Dekiteiru",
+                "AniList": "https://anilist.co/anime/21769/Yahari-Ore-no-Seishun-Love-Come-wa-Machigatteiru-Zoku-Kitto-Onnanoko-wa-Osatou-to-Spice-to-Suteki-na-Nanika-de-Dekiteiru/",
+                "MyAnimeList": "https://myanimelist.net/anime/33161/Yahari_Ore_no_Seishun_Love_Comedy_wa_Machigatteiru_Zoku_OVA"
+            },
+            {
+                "title": "Yahari Ore no Seishun Love Come wa Machigatteiru. Kan",
+                "AniList": "https://anilist.co/anime/108489/Yahari-Ore-no-Seishun-Love-Come-wa-Machigatteiru-Kan/",
+                "MyAnimeList": "https://myanimelist.net/anime/39547/Yahari_Ore_no_Seishun_Love_Comedy_wa_Machigatteiru_Kan"
+            },
+            {
+                "title": "Yahari Ore no Seishun Love Come wa Machigatteiru. Kan OVA",
+                "AniList": "https://anilist.co/anime/128643/Yahari-Ore-no-Seishun-Love-Come-wa-Machigatteiru-Kan-OVA/",
+                "MyAnimeList": "https://myanimelist.net/anime/46431/Yahari_Ore_no_Seishun_Love_Comedy_wa_Machigatteiru_Kan_OVA"
+            }
+        ]
+    },
+    {
+        "name": "Yuru Yuri",
+        "tags": ["ゆるゆり","YuruYuri"],
+        "list": [
+            {
+                "title": "Yuru Yuri",
+                "AniList": "https://anilist.co/anime/10495/Yuru-Yuri/",
+                "MyAnimeList": "https://myanimelist.net/anime/10495/Yuru_Yuri"
+            },
+            {
+                "title": "Yuru Yuri: Doushite☆Tomaranai☆Tokimeki☆Doki Doki☆Paradox☆Eternal",
+                "AniList": "https://anilist.co/anime/12665/Yuru-Yuri-DoushiteTomaranaiTokimekiDoki-DokiParadoxEternal/",
+                "MyAnimeList": "https://myanimelist.net/anime/12665/Yuru_Yuri__Doushite_Tomaranai_Tokimeki_Dokidoki_Paradox_Eternal"
+            },
+            {
+                "title": "Yuru Yuri♪♪",
+                "AniList": "https://anilist.co/anime/12403/Yuru-Yuri/",
+                "MyAnimeList": "https://myanimelist.net/anime/12403/Yuru_Yuri♪♪"
+            },
+            {
+                "title": "Yuru Yuri Nachuyachumi!",
+                "AniList": "https://anilist.co/anime/20625/Yuru-Yuri-Nachuyachumi/",
+                "MyAnimeList": "https://myanimelist.net/anime/23225/Yuru_Yuri_Nachuyachumi"
+            },
+            {
+                "title": "Yuru Yuri Nachuyachumi!+",
+                "AniList": "https://anilist.co/anime/21214/Yuru-Yuri-Nachuyachumi/",
+                "MyAnimeList": "https://myanimelist.net/anime/30902/Yuru_Yuri_Nachuyachumi_"
+            },
+            {
+                "title": "Yuru Yuri San☆Hai!",
+                "AniList": "https://anilist.co/anime/21088/Yuru-Yuri-SanHai/",
+                "MyAnimeList": "https://myanimelist.net/anime/30279/Yuru_Yuri_San☆Hai"
+            },
+            {
+                "title": "Yuru Yuri San☆Hai! Happy Go Lucky",
+                "YouTube": "https://www.youtube.com/playlist?list=PLLPYMaP0tgFJxb0NNM_Fn5V_jzKVTHf9j"
+            },
+            {
+                "title": "Yuru Yuri,",
+                "AniList": "https://anilist.co/anime/102090/Yuru-Yuri/",
+                "MyAnimeList": "https://myanimelist.net/anime/37773/Yuru_Yuri"
+            },
+            {
+                "title": "Mini Yuri",
+                "AniList": "https://anilist.co/anime/110382/Mini-Yuri/",
+                "MyAnimeList": "https://myanimelist.net/anime/40369/Mini_Yuri"
+            }
+        ]
+    }
+];
