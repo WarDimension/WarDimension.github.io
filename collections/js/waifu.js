070ff58c652bb620c591164fa10911a635382b58
WarDimension <49086905+WarDimension@users.noreply.github.com>
2022-07-21 18:10:12 +0800

    Update waifu.js

--- collections/js/waifu.js
+++ collections/js/waifu.js
@@ -16,6 +16,17 @@ function getFirstLink(girl){
     return link;
 }
 
+function isTag(tags, regex){
+    let isTag = 0;
+    tags.forEach(tag => {
+        if(tag.match(regex)){
+            isTag++;
+        }
+    });
+
+    return isTag > 0 ? true : false;
+}
+
 function showGirls(show, waifu, showAll = false){
     isFound = false;
     let contentTemp = "";
@@ -23,7 +34,7 @@ function showGirls(show, waifu, showAll = false){
     console.log(regex);
     contentTemp += "<ol>";
     waifu.forEach(girl => {
-        let isGirlExist = girl.native.match(regex) || girl.romaji?.match(regex) || (girl.tags != undefined && girl.tags.join(" ").match(regex));
+        let isGirlExist = girl.native.match(regex) || girl.romaji?.match(regex) || (girl.tags != undefined && isTag(girl.tags, regex));
         if((girl[database.value + "Link"] != undefined || !hideCheckbox.checked) && (search.value == null || isGirlExist || showAll)){
             let link = girl[database.value + "Link"] != undefined ? `<a href="${girl[database.value + "Link"]}" target="_blank">` : getFirstLink(girl);
             let romaji = girl.romaji != undefined ? `<span>${girl.romaji}</span>` : "";
@@ -36,7 +47,7 @@ function showGirls(show, waifu, showAll = false){
     if(isFound){
         content += `<p>${show.name}</p>` + contentTemp + "</ol>";
     }
-    else if((show.name.match(regex) || (show.tags != undefined && show.tags.join(" ").match(regex))) && (!hideCheckbox.checked || (JSON.stringify(waifu).match(new RegExp(database.value + "Link", "m"))))){
+    else if((show.name.match(regex) || (show.tags != undefined && isTag(show.tags, regex))) && (!hideCheckbox.checked || (JSON.stringify(waifu).match(new RegExp(database.value + "Link", "m"))))){
         showGirls(show, waifu, true);
     }
 }
