7ebc1baddbcf1f3a9a2615b639295d1ffd3bc575
WarDimension <49086905+WarDimension@users.noreply.github.com>
2022-08-03 13:10:44 +0800

    Update trailer.js

--- rebuild/js/trailer.js
+++ rebuild/js/trailer.js
@@ -1,43 +1,43 @@
 const tag = document.createElement('script');
 
 tag.src = "https://www.youtube.com/iframe_api";
-const firstScriptTag = document.getElementsByTagName('script')[0];
+const firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 let player;
 function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    height: '390',
-    width: '640',
-    videoId: 'XjD05_I7X4w',
-    playerVars: {
-      'rel': 0,
-      'controls': 0
-    },
-    events: {
-      'onStateChange': onPlayerStateChange
-    }
-  });
+    player = new YT.Player("player", {
+        height: "390",
+        width: "640",
+        videoId: "XjD05_I7X4w",
+        playerVars: {
+            "rel": 0,
+            "controls": 0
+        },
+        events: {
+            "onStateChange": onPlayerStateChange
+        }
+    });
 }
 
 let play = false;
 
 function playAndStopVideo(){
     if(play){
-      player.stopVideo();
-      play = false;
-      return;
+        player.stopVideo();
+        play = false;
+        return;
     }
 
     player.playVideo();
 }
 
 function onPlayerStateChange(event){
-  if(event.data == YT.PlayerState.ENDED){
-    player.stopVideo();
-    play = false;
-  }
-  else if(event.data == YT.PlayerState.PLAYING){
-    play = true;
-  }
+    if(event.data == YT.PlayerState.ENDED){
+        player.stopVideo();
+        play = false;
+    }
+    else if(event.data == YT.PlayerState.PLAYING){
+        play = true;
+    }
 }
