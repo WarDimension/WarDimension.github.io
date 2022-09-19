9025b12fb76ca6efc9438e052b6a9dbcaf253f4b
WarDimension <syaniavihira@gmail.com>
2022-09-19 16:37:50 +0800

    Update Gamehag TV Zone Autoplay.js

--- MyBackup/Tampermonkey/Gamehag TV Zone Autoplay.js
+++ MyBackup/Tampermonkey/Gamehag TV Zone Autoplay.js
@@ -20,6 +20,69 @@
     const result = document.querySelector(".result");
     const preroll = document.querySelector("#preroll");
 
+    //Result
+    const observer = new MutationObserver(function(mutationList){
+        for(let mutation of mutationList){
+            setTimeout(function () {
+                if(result.className == "result is-visible"){
+                    document.querySelector(".result__primary").click();
+                }
+            }, 5000);
+        }
+    });
+
+    //Banner
+    const prerollObserver = new MutationObserver(function(mutationList){
+        for(let mutation of mutationList){
+            setTimeout(function () {
+                const cpmsvideoclosebanner = document.querySelector(".cpmsvideoclosebanner");
+                if(cpmsvideoclosebanner != null){
+                    cpmsvideoclosebanner.click();
+                }
+            }, 5000);
+        }
+    });
+
+    observer.observe(result, {
+        attributes: true,
+        childList: true,
+        subtree: true
+    });
+
+    prerollObserver.observe(preroll, {
+        attributes: true,
+        childList: true,
+        subtree: true
+    });
+})();
+
+/*
+
+DAY LIMIT CHECK VERSION
+
+
+// ==UserScript==
+// @name         Gamehag TV Zone Autoplay
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Autoplay TV Zone ads on Gamehag
+// @author       WarDimension
+// @match        https://gamehag.com/tv-zone
+// @icon         https://www.google.com/s2/favicons?domain=gamehag.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    setTimeout(function () {
+        document.querySelector(".video__play").click();
+    }, 5000);
+
+    const result = document.querySelector(".result");
+    const preroll = document.querySelector("#preroll");
+
     //Banner
     const prerollObserver = new MutationObserver(function(mutationList){
         for(let mutation of mutationList){
@@ -61,3 +124,5 @@
         subtree: true
     });
 })();
+
+*/
