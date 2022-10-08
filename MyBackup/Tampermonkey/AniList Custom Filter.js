af4de4a51e78b1e275c85f427ebd516bbeca42a7
WarDimension <syaniavihira@gmail.com>
2022-10-08 15:28:10 +0800

    Update AniList Custom Filter.js

--- MyBackup/Tampermonkey/AniList Custom Filter.js
+++ MyBackup/Tampermonkey/AniList Custom Filter.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Custom filter for AniList search
 // @author       WarDimension
-// @match        https://anilist.co/search/anime
+// @match        https://anilist.co/search/anime*
 // @icon         https://www.google.com/s2/favicons?domain=anilist.co
 // @grant        none
 // ==/UserScript==
