bbe9e72310948d2f87c30621c692b25863259f8d
WarDimension <syaniavihira@gmail.com>
2023-01-01 02:15:31 +0800

    Update original.js

--- js/original.js
+++ js/original.js
@@ -663,6 +663,10 @@ const songsData = [
     "title": "いつでも、どこでも、何でもいい",
     "alt": "itsudemo-dokodemo-nandemoii",
     "img": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/db9b0a59-1832-436d-be35-c4da781cf1c8/dfkmj89-094a8df4-1534-45a7-8c0c-173d2bf0da78.png/v1/fill/w_400,h_400,q_80,strp/itsudemo__dokodemo__nandemoii_by_wardimension_dfkmj89-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NDAwIiwicGF0aCI6IlwvZlwvZGI5YjBhNTktMTgzMi00MzZkLWJlMzUtYzRkYTc4MWNmMWM4XC9kZmttajg5LTA5NGE4ZGY0LTE1MzQtNDVhNy04YzBjLTE3M2QyYmYwZGE3OC5wbmciLCJ3aWR0aCI6Ijw9NDAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.PuJVYOlFDsmRmG842REGC0Ss-_437QpoWjlooRvN1dQ",
+    "url": [{
+      "spotify": "https://open.spotify.com/album/21RyXRVHeZ8F91Uh1celSW?si=MnlajCYSSz-fUB5d2HX4cw",
+      "youtubeMusic": "https://music.youtube.com/playlist?list=OLAK5uy_n5OzWJCWwPGMaUBtCvk0gJJMZN3EYkhNU"
+    }],
     "track": [
       {
         "title": "喉が渇きました、飲み物が欲しい",
