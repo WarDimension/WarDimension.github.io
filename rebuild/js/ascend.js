5b06e091034b3a1fc6d4c42e18d1ae2f9590bebf
WarDimension <49086905+WarDimension@users.noreply.github.com>
2023-07-10 15:24:51 +0800

    delete rebuild

--- rebuild/js/ascend.js
+++
@@ -1,7 +0,0 @@
-if(localStorage.getItem("ascend") != "true" && localStorage.getItem("ascend1") != "true"){
-    window.open("https://wardimension.github.io/access-denied","_top");
-}
-else{
-    localStorage.setItem("ascend", "false");
-    localStorage.setItem("ascend1", "false");
-}
