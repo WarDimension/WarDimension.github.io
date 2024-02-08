6c1e9d68a3cd331cd71bc6eb7e439f32252e3cb3
WarDimension <syaniavihira@gmail.com>
2024-02-09 03:01:06 +0800

    Streamlabs Backup

---
+++ MyBackup/Streamlabs/Chat Box/script.js
@@ -0,0 +1,34 @@
+// Please use event listeners to run functions.
+document.addEventListener('onLoad', function(obj) {
+	// obj will be empty for chat widget
+	// this will fire only once when the widget loads
+});
+
+document.addEventListener('onEventReceived', function(obj) {
+  	// obj will contain information about the event
+    let msg = document.querySelectorAll(".message");
+        console.log(msg[msg.length-1].innerText);
+    if(msg[msg.length-1].innerText.replaceAll(" ", "") == ""){
+        let emotes = msg[msg.length-1].querySelectorAll(".emote");
+        emotes.forEach(emote => {
+            emote.style.backgroundImage = emote.style.backgroundImage.replace("1.0", "3.0");
+            emote.style.display = "inline-block";
+
+            let img = emote.firstChild;
+
+            img.src = img.src.replace("1.0", "3.0");
+
+            switch(emotes.length){
+                case 1:
+                    img.style.height = "6em";
+                    break;
+                case 2:
+                    img.style.height = "3em";
+                    break;
+                default:
+                    img.style.height = "2em";
+                    break;
+            }
+        })
+    }
+});
