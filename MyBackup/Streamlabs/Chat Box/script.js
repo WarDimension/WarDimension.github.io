af76b2734d536b3e576b4f52b073e132eef0d9c8
WarDimension <syaniavihira@gmail.com>
2024-02-11 11:50:16 +0800

    update streamlab chatbox

--- MyBackup/Streamlabs/Chat Box/script.js
+++ MyBackup/Streamlabs/Chat Box/script.js
@@ -7,28 +7,53 @@ document.addEventListener('onLoad', function(obj) {
 document.addEventListener('onEventReceived', function(obj) {
   	// obj will contain information about the event
     let msg = document.querySelectorAll(".message");
-        console.log(msg[msg.length-1].innerText);
+
     if(msg[msg.length-1].innerText.replaceAll(" ", "") == ""){
         let emotes = msg[msg.length-1].querySelectorAll(".emote");
         emotes.forEach(emote => {
+            let img = emote.firstChild;
+
             emote.style.backgroundImage = emote.style.backgroundImage.replace("1.0", "3.0").replace("1x", "3x");
             emote.style.display = "inline-block";
 
-            let img = emote.firstChild;
-
             img.src = img.src.replace("1.0", "3.0").replace("1x", "3x");
+            img.setAttribute("fetchpriority", "");
+            img.style.height = 0;
+            img.style.transition = "height 0.3s cubic-bezier(.25,.8,.25,1.1)";
 
-            switch(emotes.length){
-                case 1:
-                    img.style.height = "6em";
-                    break;
-                case 2:
-                    img.style.height = "3em";
-                    break;
-                default:
-                    img.style.height = "2em";
-                    break;
+            emote.style.opacity = 0;
+            emote.style.transition = "opacity 0.3s cubic-bezier(.25,.8,.25,1.1)";
+
+            let message = emote.parentElement;
+
+            message.style.height = 0;
+            message.style.padding = 0;
+            message.style.transition = "height 0.3s cubic-bezier(.25,.8,.25,1.1)";
+            message.style.marginBottom = "30px";
+
+            img.onload = function(){
+                switch(emotes.length){
+                    case 1:
+                        img.style.height = "6em";
+                        break;
+                    case 2:
+                        img.style.height = "3em";
+                        break;
+                    default:
+                        img.style.height = "2em";
+                        break;
+                }
+                emote.style.opacity = 1;
+
+                message.style.height = "auto";
+                message.style.padding = "15px 40px";
+                message.style.paddingTop = "35px";
+                message.style.marginBottom = 0;
             }
         })
     }
+
+    let badges = document.querySelectorAll(".badges");
+    let badgeImg = badges[badges.length-1].firstChild;
+    console.log(badgeImg);
 });
