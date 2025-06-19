0a776cdbdccc246464cc7202fbf55f0f471d30d1
WarDimension <syaniavihira@gmail.com>
2025-06-19 08:02:11 +0800

    Update viewport.js

--- js/viewport.js
+++ js/viewport.js
@@ -1,12 +1,16 @@
 var mvp = document.getElementById("vp");
-if(screen.width < 500) {
-  mvp.setAttribute("content","width=500, user-scalable=no");
-}
 
-window.addEventListener("resize", (e) => {
+function screenScale(){
   if(screen.width < 500) {
     mvp.setAttribute("content","width=500, user-scalable=no");
+  }else if(screen.width > 1920) {
+    mvp.setAttribute("content","width=1920, user-scalable=no");
   }else{
     mvp.setAttribute("content","width=device-width, initial-scale=1.0, user-scalable=no");
   }
+}
+screenScale();
+
+window.addEventListener("resize", (e) => {
+  screenScale();
 });
