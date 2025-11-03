04e334a7b7bbec54ebb41f66a9c07e2ba938fca3
WarDimension <syaniavihira@gmail.com>
2025-11-03 19:29:48 +0800

    ふりがな -reader-

--- project/furigana-reader/script.js
+++ project/furigana-reader/script.js
@@ -1,12 +1,47 @@
-let text = `この 森[もり]の 秘密[ひみつ]はなんだ？
-知[し]らないのか？`;
-
 const textElement = document.querySelector(".text");
 
-text = text.replaceAll(
-  /(\n|| )([^\[\]\n ]+)\[([^\[\]]+)\]/g,
-  (match, prefix, base, ruby) =>
-    (prefix === '\n' ? '<br>' : '') + `<ruby>${base}<rt>${ruby}</rt></ruby>`
-);
+function rubyConverter(text){
+    return text.replaceAll(
+        /(\n|| )([^\[\]\n ]+)\[([^\[\]]+)\]/g,
+        (match, prefix, base, ruby) =>
+        (prefix === '\n' ? '<br>' : '') + `<ruby>${base}<rt>${ruby}</rt></ruby>`
+    );
+}
 
-textElement.innerHTML = text;
+const dropzone = document.querySelector(".dropzone");
+const fileInput = document.querySelector(".fileInput");
+
+dropzone.addEventListener("click", () => fileInput.click());
+
+fileInput.addEventListener("change", () => {
+  if (fileInput.files.length > 0) {
+    handleFile(fileInput.files[0]);
+  }
+});
+
+// DRAG & DROP
+dropzone.addEventListener("dragover", e => {
+  e.preventDefault();
+  dropzone.classList.add("dragover");
+});
+
+dropzone.addEventListener("dragleave", () => {
+  dropzone.classList.remove("dragover");
+});
+
+dropzone.addEventListener("drop", e => {
+  e.preventDefault();
+  dropzone.classList.remove("dragover");
+  if (e.dataTransfer.files.length > 0) {
+    handleFile(e.dataTransfer.files[0]);
+  }
+});
+
+// FILE
+function handleFile(file) {
+  const reader = new FileReader();
+  reader.onload = e => {
+    textElement.innerHTML = rubyConverter(e.target.result);
+  };
+  reader.readAsText(file);
+}
