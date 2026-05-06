f75a94a57d37605f0009b1fa0c0ba0fe5134838c
WarDimension <syaniavihira@gmail.com>
2026-05-06 18:48:03 +0800

    Update 禅_key for note.com.js

--- MyBackup/Tampermonkey/禅_key for note.com.js
+++ MyBackup/Tampermonkey/禅_key for note.com.js
@@ -486,7 +486,6 @@
     });
 
     function updateInputSize(){
-        typingInput.style.width = typingCheck.offsetWidth > 0 ? typingCheck.offsetWidth + "px" : "";
         typingInput.style.height = "auto";
         typingInput.style.height = typingInput.scrollHeight + "px";
     }
